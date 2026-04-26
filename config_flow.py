8c924aee026506a9f7ede0b59c7d26856befb923
mshuflin <mshuflin@gmail.com>
2026-04-26 12:40:58 +0800

    refactor: remove redundant config_entry initialization from OptionsFlowHandler

--- config_flow.py
+++ config_flow.py
@@ -63,13 +63,10 @@ class CanvasConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @callback
     def async_get_options_flow(config_entry: config_entries.ConfigEntry) -> config_entries.OptionsFlow:
         """Create the options flow."""
-        return OptionsFlowHandler(config_entry)
+        return OptionsFlowHandler()
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
     """Options Flow Class"""
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
